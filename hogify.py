7386da6cc171fc4b94944074188d741e4c5aece7
Tanner Caffrey <57373007+tanner-caffrey@users.noreply.github.com>
2023-02-20 22:01:11 +0000

    Object oriented programming, babeyy

--- hogify.py
+++ hogify.py
@@ -10,96 +10,125 @@ import logging
 import os
 import random
 import shutil
+from dataclasses import dataclass
 import requests
 
 
-# Assets directory; if it does not exist, gathers resources from online
-ASSETS_DIR = "assets"
-# Location in which to download and release the feral hogs
-YOUR_YARD = ""
-# Local hog photo directory in assets directory
-HOGSTY = "hogpics"
-# Local names file in assets directory
-ROSTER = "names.json"
-# Remote location of hog photos
-REMOTE_HOGSTY = "https://api.github.com/repos/tanner-caffrey/hogify/contents/assets/hogpics"
-# Remote location of names
-REMOTE_NAMES = "https://raw.githubusercontent.com/dominictarr/random-name/master/first-names.json"
+@dataclass
+class LocalConfig:
+    """Local configuration"""
+    assets_dir: str = "assets"
+    hogsty:     str = "hogpics"
+    roster:     str = "names.json"
 
-# Enable to turn off writing data
-SPOOF = True
-if SPOOF :
-    print("SPOOFing ON.")
+@dataclass
+class RemoteConfig:
+    """Remote configuration"""
+    hogsty:  str ="https://api.github.com/repos/tanner-caffrey/hogify/contents/assets/hogpics"
+    roster:   str ="https://raw.githubusercontent.com/dominictarr/random-name/master/first-names.json"
 
-# Forces the use of remote name and hog photo lists
-FORCE_REMOTE = True
+@dataclass
+class RunConfig:
+    """Runtime configuration"""
+    spoof: bool = True
+    debug: bool = True
+    remote: bool = False
+    your_yard: str = ""
 
-# Set logging level based on verbosity
-log = logging.getLogger(__name__)
-ch = logging.StreamHandler()
-log.addHandler(ch)
+@dataclass
+class Config:
+    """Hogify configuration"""
+    local: LocalConfig = LocalConfig()
+    remote: RemoteConfig = RemoteConfig()
+    runtime: RunConfig = RunConfig()
 
-def get_hogs(local_hogs_in_your_area: bool):
-    """Wrangles the local hogs in your area or goes on an expedition to git for them"""
-    if local_hogs_in_your_area:
-        return get_local_hogs()
-    return download_hogs()
+class Hogify:
+    """Hogify"""
+            
+    def __init__(self, config = Config()):
+        self.config = config
+        self.log = logging.getLogger(__name__)
+        self.log.addHandler(logging.StreamHandler())
 
-# free hog pics in your area
-def download_hogs():
-    """Downloads hogs from github to local memory"""
-    log.info("Wrangling remote hogs.")
-    r = requests.get(REMOTE_HOGSTY)
-    hog_data = r.json()
-    hog_urls = [(hog["name"], hog["download_url"]) for hog in hog_data]
-    hogs = {hog[0]: requests.get(hog[1]).content for hog in hog_urls}  # uh oh can a list contain that many entire hogs
-    return hogs
+    def get_hogs(self, local_hogs_in_your_area: bool):
+        """Wrangles the local hogs in your area or goes on an expedition to git for them"""
+        if local_hogs_in_your_area:
+            return self.get_local_hogs()
+        return self.download_hogs()
 
-def get_local_hogs():
-    """Gets the local hogs in your area if they exist"""
-    log.info("Getting local hogs.")
-    pics_path = os.path.join(ASSETS_DIR,HOGSTY)
-    pics = [f for f in os.listdir(pics_path) if "hog" in f and "py" not in f]
-    return pics
+    # free hog pics in your area
+    def download_hogs(self):
+        """Downloads hogs from github to local memory"""
+        self.log.info("Wrangling remote hogs.")
+        resp = requests.get(self.config.remote.hogsty)
+        hog_data = resp.json()
+        hog_urls = [(hog["name"], hog["download_url"]) for hog in hog_data]
+        hogs = {hog[0]: requests.get(hog[1]).content for hog in hog_urls}  # uh oh can a list contain that many entire hogs
+        return hogs
 
-def get_names(local_hogs_in_your_area: bool):
-    """Gets list of names based on if local config exists"""
-    if local_hogs_in_your_area:
-        return get_local_names()
-    return download_names()
+    def get_local_hogs(self):
+        """Gets the local hogs in your area if they exist"""
+        self.log.info("Getting local hogs.")
+        pics_path = os.path.join(self.config.local.assets_dir,self.config.local.hogsty)
+        pics = [f for f in os.listdir(pics_path) if "hog" in f and "py" not in f]
+        return pics
 
-def download_names():
-    """Gets list of names from git"""
-    log.info("Downloading names.")
-    r = requests.get(REMOTE_NAMES)
-    names = list(r.json())
-    return names
+    def get_names(self,local_hogs_in_your_area: bool):
+        """Gets list of names based on if local config exists"""
+        if local_hogs_in_your_area:
+            return self.get_local_names()
+        return self.download_names()
 
-def get_local_names():
-    """Gets list of names from config folder"""
-    log.info("Getting local names.")
-    names_path = os.path.join(ASSETS_DIR,ROSTER)
-    with open(names_path) as f:
-        return list(json.load(f))
+    def download_names(self):
+        """Gets list of names from git"""
+        self.log.info("Downloading names.")
+        resp = requests.get(self.config.remote.roster)
+        names = list(resp.json())
+        return names
 
-def download_feral_hog(local_hogs_in_your_area: bool, name: str, hogs):
-    """Either copies local hogs or writes the locally stored hogs to the yard"""
-    if SPOOF:
-        log.debug("Theoretically releasing hog! Say hola to %s!", name)
-        return
-    if local_hogs_in_your_area:
-        log.debug("Copying hog!! Say hello to %s!", name)
-        pic = os.path.join(ASSETS_DIR,HOGSTY,random.choice(hogs))
-        pic_ext = "."+pic.split('.')[1]
-        destination = os.path.join(YOUR_YARD,name+pic_ext)
-        shutil.copy(pic,destination)
-    else:
-        log.debug("Writing hog!! Say hello to %s!", name)
-        hog = random.choice(list(hogs.keys()))
-        pic_ext = "."+hog.split('.')[1] # split hog
-        destination = os.path.join(YOUR_YARD,name+pic_ext)
-        with open(destination, 'wb') as handler:
-            handler.write(hogs.get(hog)) # write!! that!! hog!!
+    def get_local_names(self):
+        """Gets list of names from config folder"""
+        self.log.info("Getting local names.")
+        names_path = os.path.join(self.config.local.assets_dir,self.config.local.roster)
+        with open(names_path) as f:
+            return list(json.load(f))
+
+    def download_feral_hog(self,local_hogs_in_your_area: bool, name: str, hogs):
+        """Either copies local hogs or writes the locally stored hogs to the yard"""
+        if self.config.runtime.spoof:
+            self.log.debug("Theoretically releasing hog! Say hola to %s!", name)
+            return
+        if local_hogs_in_your_area:
+            self.log.debug("Copying hog!! Say hello to %s!", name)
+            pic = os.path.join(self.config.local.assets_dir,self.config.local.hogsty,random.choice(hogs))
+            pic_ext = "."+pic.split('.')[1]
+            destination = os.path.join(self.config.runtime.your_yard,name+pic_ext)
+            shutil.copy(pic,destination)
+        else:
+            self.log.debug("Writing hog!! Say hello to %s!", name)
+            hog = random.choice(list(hogs.keys()))
+            pic_ext = "."+hog.split('.')[1] # split hog
+            destination = os.path.join(self.config.runtime.your_yard,name+pic_ext)
+            with open(destination, 'wb') as handler:
+                handler.write(hogs.get(hog)) # write!! that!! hog!!
+
+    def run(self, number_of_feral_hogs):
+        """downloads 30-50 feral hogs to your yard"""
+
+        local_hogs_in_your_area = os.path.exists(self.config.local.assets_dir) and not self.config.runtime.remote
+
+        if local_hogs_in_your_area: 
+            self.log.info("Local hogs in your area. 👀") # 👀
+
+        hogs = self.get_hogs(local_hogs_in_your_area)
+        names = self.get_names(local_hogs_in_your_area)
+
+        self.log.info("Downloading %d feral hog%s.", number_of_feral_hogs, 's'*(number_of_feral_hogs>1)) # ohgod oh fjuck
+
+        for _ in range(number_of_feral_hogs):
+            name = random.choice(names)
+            self.download_feral_hog(local_hogs_in_your_area, name, hogs)
+
 
 def process_arguments():
     """Processes the flags and arguments for the running of hogify"""
@@ -136,39 +165,21 @@ def check_positive(value):
     return ivalue
 
 def main():
-    """downloads 30-50 feral hogs to your yard"""
-
+    """Run"""
     # Process arguments
     args = process_arguments()
 
-    # Set flags
-    global FORCE_REMOTE
-    FORCE_REMOTE = args.force_remote
+    hogify = Hogify()
 
     # Set logging level
     if args.debug_mode:
-        log.setLevel(logging.DEBUG)
-        log.debug("Debug mode ON.")
+        hogify.log.setLevel(logging.DEBUG)
+        hogify.log.debug("Debug mode ON.")
     elif args.verbose:
-        log.setLevel(logging.INFO)
+        hogify.log.setLevel(logging.INFO)
 
+    hogify.run(args.number_of_feral_hogs)
 
-    local_hogs_in_your_area = os.path.exists(ASSETS_DIR) and not FORCE_REMOTE
-
-    if local_hogs_in_your_area: 
-        log.info("Local hogs in your area. 👀") # 👀
-
-    hogs = get_hogs(local_hogs_in_your_area)
-    names = get_names(local_hogs_in_your_area)
-
-    # how many hogs?
-    number_of_feral_hogs = args.number_of_feral_hogs
-
-    log.info("Downloading %d feral hog%s.", number_of_feral_hogs, 's'*(number_of_feral_hogs-1)) # ohgod oh fjuck
-
-    for _ in range(number_of_feral_hogs):
-        name = random.choice(names)
-        download_feral_hog(local_hogs_in_your_area, name, hogs)
 
 # run those hogs
 if __name__ == "__main__":
